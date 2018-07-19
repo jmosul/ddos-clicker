f22cbf937310b5b88013a58dba7c4e94cd1d4092
james <jmosul@gmail.com>
2018-07-19 21:29:30 +0100

    feat: add vue-vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <DataCenter/>
+      <v-container fluid class="py-0 px-0">
+        <v-layout align-center justify-center row fill-height>
+          <v-flex>
+            <DataCenter/>
+          </v-flex>
+          <v-flex>
+            INTERACT
+          </v-flex>
+          <v-flex>
+            DATA CENTER 2
+          </v-flex>
+        </v-layout>
+      </v-container>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import DataCenter from '@/components/DataCenter.vue'; // @ is an alias to /src
+    import {Component, Vue} from 'vue-property-decorator';
+    import DataCenter from '@/components/DataCenter.vue'; // @ is an alias to /src
 
-@Component({
-  components: {
-      DataCenter,
-  },
-})
-export default class Home extends Vue {}
+    @Component({
+        components: {
+            DataCenter,
+        },
+    })
+    export default class Home extends Vue {
+    }
 </script>
